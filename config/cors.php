9a49b1e39e3d9ef4cef803cba9333db10d19b3bc
chi-yh <chi802827@gmail.com>
2025-08-19 11:16:15 +0800

    cors調整

--- config/cors.php
+++ config/cors.php
@@ -2,6 +2,8 @@
 // JOIKA_PHP/cors.php
 
 $allowed_origins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5174',
   'http://localhost:5175',
