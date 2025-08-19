fbefe32a61f3ec5a7145444bf9b33deb950bf32a
chi-yh <chi802827@gmail.com>
2025-08-19 11:08:06 +0800

    跨域調整

--- config/cors.php
+++ config/cors.php
@@ -2,10 +2,24 @@
 // JOIKA_PHP/cors.php
 
 $allowed_origins = [
-  'http://localhost:5173',
-  'http://127.0.0.1:5173',
-  'http://localhost:5500',
-  'http://127.0.0.1:5500',
+  'http://localhost:5174',
+  'http://127.0.0.1:5174',
+  'http://localhost:5175',
+  'http://127.0.0.1:5175',
+  'http://localhost:5176',
+  'http://127.0.0.1:5176',
+  'http://localhost:5177',
+  'http://127.0.0.1:5177',
+  'http://localhost:5178',
+  'http://127.0.0.1:5178',
+  'http://localhost:5179',
+  'http://127.0.0.1:5179',
+  'http://localhost:5180',
+  'http://127.0.0.1:5180',
+  'http://localhost:5181',
+  'http://127.0.0.1:5181',
+  'http://localhost:5182',
+  'http://127.0.0.1:5182'
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
