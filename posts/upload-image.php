c30089bd6d82e8a93b56a80dbbb669a3e7c6aeb1
ZZJ <miro37091@gmail.com>
2025-08-27 16:21:21 +0800

    文章圖片上傳更新

--- posts/upload-image.php
+++ posts/upload-image.php
@@ -5,10 +5,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//  header("Access-Control-Allow-Origin: *");
+ header("Access-Control-Allow-Origin: *");
  header("Content-Type: application/json; charset=UTF-8");
 
-    // require_once __DIR__ . '/../config/cors.php';
+    require_once __DIR__ . '/../config/cors.php';
     require_once __DIR__ . '/../config/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
